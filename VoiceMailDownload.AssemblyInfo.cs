fe59703ff1eac911cde78cfe61ea473f4e673edf
publius_cp <SND\publius_cp>
2007-11-12 14:54:57 +0000

    initial check in, fully operational program.

---
+++ VoiceMailDownload.AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VoiceMailDownload")]
+[assembly: AssemblyDescription("A utility to download Voice Mail files from GalaxyVoice.Com")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("George J. Carrette")]
+[assembly: AssemblyProduct("VoiceMailDownload")]
+[assembly: AssemblyCopyright("Copyright © 2007 George J. Carrette")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
